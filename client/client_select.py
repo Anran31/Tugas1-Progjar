5a7f8c6bdf689c9a03f7d4b5a7f869b67a80f56c
Anran31 <andika.nugrahanto2002@gmail.com>
2022-03-03 16:25:10 +0700

    finish using select

--- client/client_select.py
+++ client/client_select.py
@@ -4,49 +4,53 @@ import sys
 
 BUFFER_SIZE = 1024
 
-def download_file(socket ,filename):
+def download_file(socket ,filename, file_size, header_size):
     with open(filename, 'wb') as file:
-        counter = 0
+        total = 0
+        header = True
         while True:
-            # read 1024 bytes from the socket (receive)
-            print('receiving.....', counter)
-            counter += 1
             bytes_read = socket.recv(BUFFER_SIZE)
-            if bytes_read == bytes('EOF','utf-8'):
-                # nothing is received
-                # file transmitting is done
-                print('break')
+            total += len(bytes_read)
+            if header:
+                file.write(bytes_read[header_size:])
+                header = False
+            else:
+                file.write(bytes_read)
+            if (file_size + header_size) == total:
+                print(f'Finished receiving {filename}')
                 break
-            file.write(bytes_read)
-        # write to the file the bytes we just received
         file.close()
 
-server_address = ('172.20.28.238', 5000)
+server_address = ('127.0.0.1', 5000)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
 
 # sys.stdout.write('>> ')
-print('>> ',end='')
-with open('apa.txt', 'wb') as file:
-    file.write(b'tes')
-    file.close()
+print('>> ',end='', flush=True)
 
 try:
     while True:
         message = sys.stdin.readline()
         split_message = message.strip().split()
-        print(message)
-        # message = input('>> ')
+
         client_socket.send(bytes(message, 'utf-8'))
-        # client_socket.send(message.encode())
+
         if split_message[0].lower() == 'unduh':
-            download_file(client_socket, ' '.join(split_message[1:]))
+            file_size = client_socket.recv(BUFFER_SIZE).decode('utf-8')
+            client_socket.send(bytes('file size received','utf-8'))
+
+            if file_size != '0':
+                header_size = client_socket.recv(BUFFER_SIZE).decode('utf-8')
+                client_socket.send(bytes('header size received','utf-8'))
+                download_file(client_socket, ' '.join(split_message[1:]), int(file_size), int(header_size))
+            else:
+                received_data = client_socket.recv(BUFFER_SIZE).decode('utf-8')
+                print(received_data)
         else:
-            received_data = client_socket.recv(1024).decode('utf-8')
-        # sys.stdout.write(received_data)
-        # print(received_data)
-        print('>> ', end='')
-        # sys.stdout.write('>> ')
+            received_data = client_socket.recv(BUFFER_SIZE).decode('utf-8')
+            print(received_data)
+
+        print('>> ', end='', flush=True)
 
 except KeyboardInterrupt:
     client_socket.close()
--- server/server_select.py
+++ server/server_select.py
@@ -16,36 +16,31 @@ def connect(server_socket, input_socket):
                     input_socket.append(client_socket)        
                 
                 else:            	
-                    # data = sock.recv(1024).decode('utf-8')
                     data = sock.recv(1024)
-                    # print(data.decode('utf-8'))
                     if data:
-                        print(data)
                         split_data = data.decode('utf-8').split()
                         if len(split_data) > 1:
                             command, filename = split_data[0], ' '.join(split_data[1:])
-                            print(command, filename)
+
                             if command.lower() == 'unduh':
-                                filepath = get_file_path(filename)
-                                if filepath:
-                                    print('sending data in split_data')
-                                    # sock.sendall(f'{filename} found'.encode())
-                                    send_file(input_socket, sock, filepath)
+                                file_path, file_size = get_file_path_and_size(filename)
+                                sock.sendall(f'{file_size}'.encode())
+                                response = sock.recv(BUFFER_SIZE).decode('utf-8')
+
+                                if file_path and file_size:
+                                    header_message = f"file-name: {filename},\nfile-size: {file_size},\n\n\n"
+                                    header_size = bytes(header_message,'utf-8')
+                                    sock.sendall(bytes(str(len(header_size)),'utf-8'))
+                                    response = sock.recv(BUFFER_SIZE).decode('utf-8')
+                                    send_file(sock, file_path, header_message)
                                 else:
-                                    print('sending data in split_data')
                                     sock.sendall(f'{filename} not found'.encode())
+
                             else:
-                                print('sending data in split_data')
                                 sock.sendall(f'Unduh file dengan mengirimkan command \'unduh <nama file>\''.encode())
+
                         else:
-                            print('sending data in split_data')
                             sock.sendall(f'Unduh file dengan mengirimkan command \'unduh <nama file>\''.encode())
-                    
-                    # elif data == b'\n':
-                    #     print('sending data in elif data', data.decode('utf-8'))
-                    #     # sock.sendall(data.encode())
-                    #     sock.sendall(f'Unduh file dengan mengirimkan command \'unduh <nama file>\''.encode())
-                    #     # sock.send(data)
 
                     else:
                         print('closing socket')                    
@@ -56,35 +51,40 @@ def connect(server_socket, input_socket):
         server_socket.close()
         sys.exit(0)
 
-def get_file_path(filename):
+def get_file_path_and_size(filename):
     dataset_dir_path = os.path.join(os.getcwd(), 'dataset')
-    print(dataset_dir_path)
 
     file_path = os.path.join(dataset_dir_path, filename)
-    print(file_path)
 
     if os.path.exists(file_path):
-        return file_path
-    return ''
+        file_size = os.path.getsize(file_path)
+        return file_path, file_size
+    return '', 0
 
-def send_file(input_socket, socket, filepath):
-    with open(filepath, 'rb') as file:
+def send_file(socket, filepath, header_message):
+    temp_file_name = 'temp_' + os.path.basename(filepath)
+    with open(temp_file_name, 'wb') as file:
+        file.write(bytes(header_message, 'utf-8'))
+        with open(filepath, 'rb') as real_file:
+            file.write(real_file.read())
+            real_file.close()
+        file.close()
+
+    with open(temp_file_name, 'rb') as file:
         while True:
-            # read the bytes from the file
             bytes_read = file.read(BUFFER_SIZE)
             if not bytes_read:
-                # file transmitting is done
-                print('end of file')
-                socket.sendall(bytes('EOF','utf-8'))
+                file_name = os.path.basename(filepath)
+                print(f'Finished sending {file_name}')
                 break
             socket.sendall(bytes_read)
-            # we use sendall to assure transimission in 
-            # busy networks
         file.close()
+    
+    os.remove(temp_file_name)
 
 
 if __name__ == "__main__":
-    server_address = ('172.20.28.238', 5000)
+    server_address = ('127.0.0.1', 5000)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(server_address)
